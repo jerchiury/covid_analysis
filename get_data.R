fb1497d4f20fc9e4ac64c16cb03378251b2917ae
Jerry Chiu <jerchiurytw@gmail.com>
2021-01-20 22:35:54 -0500

    added individual level data

--- get_data.R
+++ get_data.R
@@ -2,8 +2,9 @@ library(httr)
 library(jsonlite)
 setwd('C:\\Users\\Jerry\\Desktop\\Jerry\\projects\\covid19')
 today=Sys.Date()
+cases.zip='https://www150.statcan.gc.ca/n1/pub/13-26-0003/2020001/COVID19-eng.zip' # individual leevel data source
 
-get.data=function(url, prefix, query='', write.to.file=F, file.prefix=today){
+get.data=function(url, prefix, query='', write.to.file=F, file.prefix=today){ #provincial or health region level data
   query=paste(query, collapse='&')
   url=paste0(url, '?', query)
   print(url)
@@ -17,11 +18,30 @@ get.data=function(url, prefix, query='', write.to.file=F, file.prefix=today){
     eval.text=paste0(var.name, '<<-temp$', n)
     eval(parse(text=eval.text))
     if(write.to.file){
-      file.name=paste0(file.prefix, '_', prefix, '_', n, '.csv')
+      file.name=paste0(file.prefix, '_', gsub('\\.', '_', prefix), '_', n, '.csv')
       eval.text=paste0('write.csv(', var.name, ',"', file.name, '", row.names=F)')
       eval(parse(text=eval.text))
-      }
     }
   }
-get.data('https://api.opencovid.ca/timeseries', 'ts', write.to.file=T)
+}
+
+get.ind.data=function(url, write.to.file=F){ # individual level data
+  temp <- tempfile()
+  download.file('https://www150.statcan.gc.ca/n1/pub/13-26-0003/2020001/COVID19-eng.zip',temp)
+  names=unzip(temp, list=T)$Name
+  for(n in names){
+    var.name=tolower(gsub('-eng.csv', '', n))
+    assign(var.name, read.csv(unz(temp, n)), envir=globalenv())
+    if(write.to.file){
+      eval.text=paste0('write.csv(', var.name, ',"', n, '", row.names=F)')
+      eval(parse(text=eval.text))
+      }
+  }
+  unlink(temp)
+  }
+
+get.data('https://api.opencovid.ca/timeseries', 'ts.hr', 'loc=hr')
+get.data('https://api.opencovid.ca/timeseries', 'ts.prov', 'loc=prov')
+
+get.ind.data(case.zip)
 
